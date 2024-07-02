fbe9b79b98692b0579b4b080e34b841e14287480
Maestro <78421978+dhiiiinnn7@users.noreply.github.com>
2024-07-02 21:41:52 +0100

    Add files via upload

---
+++ Final Project Files and Report/Programming/ID CARD - UNAUTHORISED.py
@@ -0,0 +1,28 @@
+from time import *
+from physical import *
+
+CARD_ID = 1002  # var CARD_ID
+
+
+def setup():
+    global CARD_ID
+    CARD_ID = restoreProperty('CardID', CARD_ID)
+    setDeviceProperty(getName(), 'CardID', CARD_ID)
+
+
+def restoreProperty(propertyName, defaultValue):
+    value = getDeviceProperty(getName(), propertyName)
+    if not (value is "" or value is None):
+        if type(defaultValue) is int:
+            value = int(value)
+
+        setDeviceProperty(getName(), propertyName, value)
+        return value
+    return defaultValue
+
+
+if __name__ == "__main__":
+    setup()
+    while True:
+        sleep(3600)
+
